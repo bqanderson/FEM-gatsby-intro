048889b6578948aa12d8dbaffb7f7d536c3c5204
bqanderson <bqanderson@me.com>
2020-02-05 15:13:49 -0600

    feat: update to optimized hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { Link, graphql, useStaticQuery } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
-const ImageBackground = styled('div')`
-  background-image: url('/images/nicole-geri-mpls.jpg');
+const ImageBackground = styled(BackgroundImage)`
   background-position: top 20% center;
   background-size: cover;
   height: 35vh;
@@ -39,8 +39,20 @@ const TextBox = styled('div')`
 `
 
 const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "nicole-geri-mpls.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <ImageBackground>
+    <ImageBackground Tag='section' fluid={image.sharp.fluid}>
       <TextBox>
         <h1>Frontend Masters + Gatsby &hearts;</h1>
         <p>
