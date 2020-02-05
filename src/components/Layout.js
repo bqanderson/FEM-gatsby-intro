07490c62421698ebe7b34f963258b6a3fff9942b
bqanderson <bqanderson@me.com>
2020-02-04 19:10:24 -0600

    feat: add site metadata + hook to access it

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Global, css } from '@emotion/core'
+import Helmet from 'react-helmet'
 import Header from './Header'
+import useSiteMetadata from '../hooks/use-sitemetadata'
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+
+  return (
+    <>
+      <Global
+        styles={css`
         * {
           box-sizing: border-box;
           margin: 0;
@@ -51,18 +56,24 @@ const Layout = ({ children }) => (
           }
         }
       `}
-    />
-    <Header />
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 90vw;
-        width: 550px;
-      `}
-    >
-      {children}
-    </main>
-  </>
-)
+      />
+      <Helmet>
+        <html lang='en' />
+        <title>{title}</title>
+        <meta name='description' content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </>
+  )
+}
 
 export default Layout
